42c7fc51ad5a77f0459875dcd53d90f683fa933a
Daylen-Mackey <dmackey@ualberta.ca>
2020-10-31 01:12:30 -0600

    Progressing through Fixed Exchange Rate module

--- ConceptDashboard/Concept 1 -- Fixed Exchange Rate Currencies/WikipediaDataScrape.py
+++ ConceptDashboard/Concept 1 -- Fixed Exchange Rate Currencies/WikipediaDataScrape.py
@@ -22,9 +22,11 @@ from bs4 import BeautifulSoup
 
 url_1 = 'https://en.wikipedia.org/wiki/List_of_circulating_currencies' # All currencies
 url_2 = 'https://en.wikipedia.org/wiki/List_of_circulating_fixed_exchange_rate_currencies' #Fixed  currencies
+url_3 = 'https://www.xe.com/symbols.php' # List of all currency symbols --> Not used 
 
 request1 = requests.get(url_1).text
 request2 = requests.get(url_2).text
+
 
 soup_1 = BeautifulSoup(request1,'lxml')
 soup_2 = BeautifulSoup(request2,'lxml')
@@ -32,6 +34,8 @@ soup_2 = BeautifulSoup(request2,'lxml')
 
 all_currencies_table = soup_1.find('table',{'class' : 'wikitable sortable' })
 fx_table = soup_2.find('table',{'class' : 'wikitable sortable' })
+
+
 #%% We've grabbed our tables, now let's convert them into DataFrames
  
 df_all_curr = pd.read_html(str(all_currencies_table))[0]
@@ -44,9 +48,28 @@ df_all_curr.columns = ['Country', 'Currency','Symbol','ISO', 'Unit','Number to B
 
 df_fx.rename({'Fixed Currency': 'Currency'},axis = 1, inplace = True)
 
-
 df_merged = df_all_curr.merge(df_fx,how = 'left', on = 'Currency')
 
-#%%
+#Mapping all the symbols 
 
-fx = df_merged.dropna().
+#making a mask of all the pegged_currencies
+
+pegged_mask = df_merged['Reference Currency'].notna()
+
+reference_currency_isos = df_merged['Reference Currency'].to_frame().merge(df_all_curr.drop_duplicates(subset = ['Currency']), how = 'left', left_on = 'Reference Currency', right_on = 'Currency').ISO
+# Pound Sterling not Recognized in our previously scraped dataframe
+
+df_merged.insert(0,'Ref_ISO',reference_currency_isos)
+
+
+GBP_mask = (df_merged['Reference Currency'] == 'Pound sterling')
+df_merged.loc[GBP_mask,'Ref_ISO'] = "GBP"
+
+
+#%% Now that we have everything we need, we can start looking at APIs
+
+
+
+
+
+
